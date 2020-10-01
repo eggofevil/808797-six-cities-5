83f7a7c2532be07bef71f636a5fc006e8639d3da
eggofevil <eggofevil@gmail.com>
2020-10-01 18:56:33 +0300

    5. Личный проект: начинаем программировать

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
 
+ReactDOM.render(
+    <App offersCount = {3}/>,
+    document.getElementById(`root`)
+);
