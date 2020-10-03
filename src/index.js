efa5880c0f6894cc9c54486678d9a47444993cb1
eggofevil <eggofevil@gmail.com>
2020-10-03 22:39:32 +0300

    7. Личный проект: маршрут по тайным тропам

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 ReactDOM.render(
-  <App offersCount = {3}/>,
+  <App offersCount = {5}/>,
   document.getElementById(`root`)
 );
