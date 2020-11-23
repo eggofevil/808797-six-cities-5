21e02e18eaef0820d529b366c5c7f102d18cfbab
eggofevil <eggofevil@gmail.com>
2020-11-23 14:40:17 +0300

    добавляем поле is_favorite в структуре моков в соответствии с предполагаемой структурой данных поступающих с сервера

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -2,225 +2,231 @@
 
 export default [
   {
-    bedrooms: 1,
-    city: {
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
+    'bedrooms': 1,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
       },
-      name: `Amsterdam`
+      'name': `Amsterdam`
     },
-    description: `Comfortable and modern multi-apartment house, situated in nice natural area. Good transport acceciblity, many places to spend a time: tennis court, workout grounds, etc`,
-    goods: [`WiFi`, `Heating`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
-    host: {
+    'description': `Comfortable and modern multi-apartment house, situated in nice natural area. Good transport acceciblity, many places to spend a time: tennis court, workout grounds, etc`,
+    'goods': [`WiFi`, `Heating`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 1,
       'is_pro': true,
       'name': `Fly`
     },
-    id: 1,
-    images: [`https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`],
-    rating: 4.1,
-    property: `Teremok`,
-    slogan: `Nice apartment among various friendly neighbors!`,
-    price: 100,
-    main: {
-      address: `some meadow in the forest`,
-      coords: [52.3909553943508, 4.85309666406198],
+    'id': 1,
+    'images': [`https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 4.1,
+    'property': `Teremok`,
+    'slogan': `Nice apartment among various friendly neighbors!`,
+    'price': 100,
+    'main': {
+      'address': `some meadow in the forest`,
+      'coords': [52.3909553943508, 4.85309666406198],
 
-      type: `apartment`
+      'type': `apartment`
     },
-    facilities: {
-      premium: false,
-      capacity: {
-        adults: 2,
-        children: 2
+    'facilities': {
+      'premium': false,
+      'capacity': {
+        'adults': 2,
+        'children': 2
       }
     }
   },
   {
-    bedrooms: 1,
-    city: {
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
+    'bedrooms': 1,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
       },
-      name: `Amsterdam`
+      'name': `Amsterdam`
     },
-    description: `Historical underground bulding, was founded in dark ages and has a dark story. Many unusual entertainments for the most demanding taste.`,
-    goods: [`WiFi`, `TV`, `big club`],
-    host: {
+    'description': `Historical underground bulding, was founded in dark ages and has a dark story. Many unusual entertainments for the most demanding taste.`,
+    'goods': [`WiFi`, `TV`, `big club`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 2,
       'is_pro': true,
       'name': `Some evil force`
     },
-    id: 2,
-    images: [`https://via.placeholder.com/260x200`],
-    rating: 4.4,
-    property: `Cell in Darkest Dungeon`,
-    slogan: `You will be frequently provided with very interesting visiters!`,
-    price: 50,
-    main: {
-      address: `castle in some forgotten land`,
-      coords: [52.369553943508, 4.85309666406198],
-      type: `room`
+    'id': 2,
+    'images': [`https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 4.4,
+    'property': `Cell in Darkest Dungeon`,
+    'slogan': `You will be frequently provided with very interesting visiters!`,
+    'price': 50,
+    'main': {
+      'address': `castle in some forgotten land`,
+      'coords': [52.369553943508, 4.85309666406198],
+      'type': `room`
     },
-    facilities: {
-      premium: false,
-      capacity: {
-        adults: 1,
-        children: 0
+    'facilities': {
+      'premium': false,
+      'capacity': {
+        'adults': 1,
+        'children': 0
       }
     }
   },
   {
-    bedrooms: 2,
-    city: {
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
+    'bedrooms': 2,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
       },
-      name: `Amsterdam`
+      'name': `Amsterdam`
     },
-    description: `Nice and fast wooden building without a fixed abode. It was built in ancient times and still preserves historical shape.`,
-    goods: [`WiFi`, `stove`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`, `navigation system`, `private flyibg transport`],
-    host: {
+    'description': `Nice and fast wooden building without a fixed abode. It was built in ancient times and still preserves historical shape.`,
+    'goods': [`WiFi`, `stove`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`, `navigation system`, `private flyibg transport`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 3,
       'is_pro': true,
       'name': `Bаba Yaga`
     },
-    id: 3,
-    images: [`https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`],
-    rating: 4.8,
-    property: `Hut on chicken legs`,
-    slogan: `Many interesting sights in stone's throw, you don't need to rent a car`,
-    price: 200,
-    main: {
-      address: `Who knows were is it in this moment?`,
-      coords: [52.3909553943508, 4.929309666406198],
-      type: `Entire house`,
+    'id': 3,
+    'images': [`https://via.placeholder.com/260x200`, `https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 4.8,
+    'property': `Hut on chicken legs`,
+    'slogan': `Many interesting sights in stone's throw, you don't need to rent a car`,
+    'price': 200,
+    'main': {
+      'address': `Who knows were is it in this moment?`,
+      'coords': [52.3909553943508, 4.929309666406198],
+      'type': `Entire house`,
     },
-    facilities: {
-      premium: true,
-      capacity: {
-        adults: 4,
-        children: 4
+    'facilities': {
+      'premium': true,
+      'capacity': {
+        'adults': 4,
+        'children': 4
       }
     }
   },
   {
-    bedrooms: 2,
-    city: {
-      location: {
-        latitude: 52.370216,
-        longitude: 4.895168,
-        zoom: 10
+    'bedrooms': 2,
+    'city': {
+      'location': {
+        'latitude': 52.370216,
+        'longitude': 4.895168,
+        'zoom': 10
       },
-      name: `Amsterdam`
+      'name': `Amsterdam`
     },
-    description: `Old historical building with unusual architecture. It's situated in wonderful natural area. Very nice children's restoraunt inside.`,
-    goods: [`WiFi`, `stove`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
-    host: {
+    'description': `Old historical building with unusual architecture. It's situated in wonderful natural area. Very nice children's restoraunt inside.`,
+    'goods': [`WiFi`, `stove`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 4,
       'is_pro': false,
       'name': `Evil Witch`
     },
-    id: 4,
-    images: [`https://via.placeholder.com/260x200`],
-    rating: 4.2,
-    property: `Gingerbread house`,
-    slogan: `Many sweets for nice children! Only for children`,
-    price: 150,
-    main: {
-      address: `Dark thicket deep in forest`,
-      coords: [52.3809553943508, 4.939309666406198],
-      type: `Entire house`
+    'id': 4,
+    'images': [`https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 4.2,
+    'property': `Gingerbread house`,
+    'slogan': `Many sweets for nice children! Only for children`,
+    'price': 150,
+    'main': {
+      'address': `Dark thicket deep in forest`,
+      'coords': [52.3809553943508, 4.939309666406198],
+      'type': `Entire house`
     },
-    facilities: {
-      premium: true,
-      capacity: {
-        adults: 0,
-        children: 4
+    'facilities': {
+      'premium': true,
+      'capacity': {
+        'adults': 0,
+        'children': 4
       }
     }
   },
   {
-    bedrooms: 1,
-    city: {
-      location: {
-        latitude: 50.8505,
-        longitude: 4.3488,
-        zoom: 10
+    'bedrooms': 1,
+    'city': {
+      'location': {
+        'latitude': 50.8505,
+        'longitude': 4.3488,
+        'zoom': 10
       },
-      name: `Brussels`
+      'name': `Brussels`
     },
-    description: `Comfortable and modern multi-apartment house, situated in nice natural area. Good transport acceciblity, many places to spend a time: tennis court, workout grounds, etc`,
-    goods: [`WiFi`, `Heating`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
-    host: {
+    'description': `Comfortable and modern multi-apartment house, situated in nice natural area. Good transport acceciblity, many places to spend a time: tennis court, workout grounds, etc`,
+    'goods': [`WiFi`, `Heating`, `kitchen`, `fridge`, `washing machine`, `towels`, `TV`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 5,
       'is_pro': false,
       'name': `Bloha poprjaduha`
     },
-    id: 5,
-    images: [`https://via.placeholder.com/260x200`],
-    rating: 3.9,
-    property: `Teremok Plaza`,
-    slogan: `Nice apartment among various friendly neighbors!`,
-    price: 100,
-    main: {
-      address: `some meadow in the forest`,
-      coords: [50.850122, 4.342151],
-      type: `apartment`
+    'id': 5,
+    'images': [`https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 3.9,
+    'property': `Teremok Plaza`,
+    'slogan': `Nice apartment among various friendly neighbors!`,
+    'price': 100,
+    'main': {
+      'address': `some meadow in the forest`,
+      'coords': [50.850122, 4.342151],
+      'type': `apartment`
     },
-    facilities: {
-      premium: false,
-      capacity: {
-        adults: 2,
-        children: 2
+    'facilities': {
+      'premium': false,
+      'capacity': {
+        'adults': 2,
+        'children': 2
       }
     }
   },
   {
-    bedrooms: 1,
-    city: {
-      location: {
-        latitude: 50.8505,
-        longitude: 4.3488,
-        zoom: 10
+    'bedrooms': 1,
+    'city': {
+      'location': {
+        'latitude': 50.8505,
+        'longitude': 4.3488,
+        'zoom': 10
       },
-      name: `Brussels`
+      'name': `Brussels`
     },
-    description: `Historical underground bulding, was founded in dark ages and has a dark story. Many unusual entertainments for the most demanding taste.`,
-    goods: [`WiFi`, `TV`, `big club`],
-    host: {
+    'description': `Historical underground bulding, was founded in dark ages and has a dark story. Many unusual entertainments for the most demanding taste.`,
+    'goods': [`WiFi`, `TV`, `big club`],
+    'host': {
       'avatar_url': `https://via.placeholder.com/74x74`,
       'id': 6,
       'is_pro': false,
       'name': `Dark Soul`
     },
-    id: 6,
-    images: [`https://via.placeholder.com/260x200`],
-    rating: 4.9,
-    property: `Cell in Darkest Dungeon`,
-    slogan: `You will be frequently provided with very interesting visiters!`,
-    price: 50,
-    main: {
-      address: `castle in some forgotten land`,
-      coords: [50.848833, 4.351776],
-      type: `room`
+    'id': 6,
+    'images': [`https://via.placeholder.com/260x200`],
+    'is_favorite': false,
+    'rating': 4.9,
+    'property': `Cell in Darkest Dungeon`,
+    'slogan': `You will be frequently provided with very interesting visiters!`,
+    'price': 50,
+    'main': {
+      'address': `castle in some forgotten land`,
+      'coords': [50.848833, 4.351776],
+      'type': `room`
     },
-    facilities: {
-      premium: false,
-      capacity: {
-        adults: 1,
-        children: 0
+    'facilities': {
+      'premium': false,
+      'capacity': {
+        'adults': 1,
+        'children': 0
       }
     }
   }
